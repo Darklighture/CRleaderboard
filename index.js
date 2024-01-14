687dcde5cac2b9f9c639b36d03ae23bfbb87ba79
Darklighture <33174501+Darklighture@users.noreply.github.com>
2024-01-14 17:32:26 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,19 +1,26 @@
-(() => {
-  const chart = new G2.Chart();
+<script src="https://unpkg.com/@antv/g2/dist/g2.min.js"></script>
+<script>
+  // 准备数据
+  const data = [
+    { genre: 'Sports', sold: 275 },
+    { genre: 'Strategy', sold: 115 },
+    { genre: 'Action', sold: 120 },
+    { genre: 'Shooter', sold: 350 },
+    { genre: 'Other', sold: 150 },
+  ];
 
-  // 一句话声明可视化
+  // 初始化图表实例
+  const chart = new G2.Chart({
+    container: 'container',
+  });
+
+  // 声明可视化
   chart
-    .point()
-    .data({
-      type: 'fetch',
-      value:
-        'https://gw.alipayobjects.com/os/basement_prod/6b4aa721-b039-49b9-99d8-540b3f87d339.json',
-    })
-    .encode('x', 'weight')
-    .encode('y', 'height')
-    .encode('color', 'gender');
+    .interval() // 创建一个 Interval 标记
+    .data(data) // 绑定数据
+    .encode('x', 'genre') // 编码 x 通道
+    .encode('y', 'sold'); // 编码 y 通道
 
+  // 渲染可视化
   chart.render();
-
-  return chart.getContainer();
-})();
+</script>
