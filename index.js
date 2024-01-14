ea83e3d62914b6bdb3d3ecab4737e4e023bc5262
Darklighture <33174501+Darklighture@users.noreply.github.com>
2024-01-14 17:26:52 +0800

    Update index.js

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-console.log("Hello Github Pages");
+(() => {
+  const chart = new G2.Chart();
+
+  // 一句话声明可视化
+  chart
+    .point()
+    .data({
+      type: 'fetch',
+      value:
+        'https://gw.alipayobjects.com/os/basement_prod/6b4aa721-b039-49b9-99d8-540b3f87d339.json',
+    })
+    .encode('x', 'weight')
+    .encode('y', 'height')
+    .encode('color', 'gender');
+
+  chart.render();
+
+  return chart.getContainer();
+})();
